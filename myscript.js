ea004505ef955dae83ca56b5496c9c367f39a81f
petre-enciu <petre202001@yahoo.com>
2024-11-06 15:24:55 +0200

    add image layout

---
+++ myscript.js
@@ -0,0 +1,11 @@
+let mapcell = document.getElementsByClassName('map-cell')[0],
+navigating_map = document.getElementsByClassName('navigating-map')[0],
+main = document.getElementsByClassName('main')[0];
+
+window.addEventListener("resize", resize);
+resize();
+function resize(){
+    
+    
+
+}
